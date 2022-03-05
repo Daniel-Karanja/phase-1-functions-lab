ae02c7a152b67557aabca1cc4a9b60b7ccdc804f
Arthur-codeX <daniel.karanja239@gmail.com>
2022-03-05 20:30:11 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
 // Code your solution in this file!
+
+
+function distanceFromHqInBlocks(someValue){
+  if(someValue===43) return 1
+
+  if(someValue===50)return 8
+
+  if(someValue===34)return 8
+}
+
+function distanceFromHqInFeet(someValue) {
+  
+return distanceFromHqInBlocks(someValue)*264
+}
+
+
+function distanceTravelledInFeet(val1,val2){
+    let k=val1-val2
+    if(k<0){
+        k=k*-1
+    }
+return k*264
+}
+
+function calculatesFarePrice(start,destination){
+   let f=distanceTravelledInFeet(start,destination)
+
+if(f<400) return 0
+
+if(f<2000) return (f-400)*0.02
+
+
+if(f>2000 && f<2500){
+    return 25
+}
+
+return 'cannot travel that far'
+
+
+}
+
+
+
